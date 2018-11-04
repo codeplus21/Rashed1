a603dc302211054d4336c1f2879e34f532ebe785
Rashed <43473387+codeplus21@users.noreply.github.com>
2018-11-05 01:58:20 +0600

    Add files via upload

---
+++ Insertation.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+//https://www.youtube.com/watch?v=2xK1l7f4p1Q ---------selection sort
+
+void insertionSort(int A[], int n){
+
+int hole,value,i;
+
+for( i=0;i<n;i++){
+
+    value= A[i];
+    hole = i;
+
+    while(hole>0 && A[hole-1]<value){
+        A[hole] = A[hole-1];
+        hole = hole -1;
+    }
+    A[hole] = value;
+
+    }
+
+}
+
+
+int main()
+ {
+    int i;
+    int A[] ={10,30,6,7,11,1};
+    insertionSort(A,6);
+    printf("In ascending order: ");
+    for(i=0;i<6;i++)
+        printf("%d  ",A[i]);
+    return 0;
+}
